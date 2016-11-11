93195594f3411b02f79f5e274ec6e071ae403153
sKiL <skil@skildust.com>
2016-11-11 18:46:08 +0100

    Neue Ladeanimation

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body class="grey {{ $colormode == 0 ? 'lighten-2 black-text' : 'darken-3 white-text'}}">
 <div style="z-index: 9999; width: 100%; height: 100%; position: fixed; background-color: rgba(0, 0, 0, 0.75);" class="loader valign-wrapper">
-    <img src="/assets/img/white-spinner.gif">
+    <img src="/assets/img/pulse.gif">
 </div>
 <noscript>
     <div style="z-index: 9999; width: 100%; height: 100%; position: fixed; background-color: #000;" class="loader valign-wrapper">
