79e90654d170b67cd071b89c3c7b749667261537
sKiL <skil@skildust.com>
2017-05-13 13:41:14 +0200

    Überflüssigen Schedule entfernt. Hat immer viel Leistung gefressen

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,21 +32,6 @@ class Kernel extends ConsoleKernel
             $config = Twitter::get('help/configuration');
             Cache::put('twitter_config', $config, 1440);
         })->daily();
-        $schedule->call(function () {
-            $accounts = DB::table('users')->get();
-            foreach($accounts as $acc) {
-                try {
-                    $token = json_decode($acc->token);
-                    Twitter::reconfig(['token' => $token->oauth_token, 'secret' => $token->oauth_token_secret]);
-                    $media = json_decode($acc->media);
-                    $avatar = Twitter::query('users/show', 'GET', ['screen_name' => $acc->handle])->profile_image_url_https;
-                    $banner = Twitter::query('users/show', 'GET', ['screen_name' => $acc->handle])->profile_banner_url;
-                    $media->avatar = $media->avatar == $avatar ? $media->avatar : $avatar;
-                    $media->banner = $media->banner == $banner ? $media->banner : $banner;
-                    DB::table('users')->where('name', $acc->name)->update(['media' => json_encode($media)]);
-                } catch (\Exception $e) {}
-            }
-        })->everyFiveMinutes();
     }
 
     /**
